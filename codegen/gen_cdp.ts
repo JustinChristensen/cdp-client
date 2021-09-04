440f10a285ef0e02884ad7adfcea3ca027a1cde1
Justin Christensen <wroathe@gmail.com>
2021-09-04 00:35:57 -0500

    WIP function exports, TODO fix code generation bug in babel

--- codegen/gen_cdp.ts
+++ codegen/gen_cdp.ts
@@ -7,14 +7,14 @@ import { writeFileSync, mkdirSync } from 'fs';
 import {
     TSType,
     program,
-    blockStatement,
-    variableDeclaration,
-    variableDeclarator,
+    callExpression as fnCall,
+    variableDeclaration as variable,
+    variableDeclarator as binding,
+    arrowFunctionExpression as arrowFunction,
     importDeclaration as importDecl,
     importNamespaceSpecifier as importNS,
     exportNamedDeclaration as exportNamed,
     identifier as ident,
-    arrowFunctionExpression,
     tsTypeAliasDeclaration as typeAlias,
     tsTypeLiteral as typeLit,
     tsPropertySignature as propSig,
@@ -23,8 +23,9 @@ import {
     tsUnionType as unionType,
     tsTypeReference as typeRef,
     tsArrayType as arrayType,
+    tsTypeParameterInstantiation as typeParams,
     tsStringKeyword, tsNumberKeyword, tsBooleanKeyword,
-    stringLiteral, numberLiteral, tsAnyKeyword, tsObjectKeyword
+    stringLiteral, tsUnknownKeyword
 } from '@babel/types';
 import generate from '@babel/generator';
 
@@ -36,16 +37,16 @@ const typesToKWs = {
     'number': tsNumberKeyword,
     'integer': tsNumberKeyword,
     'boolean': tsBooleanKeyword,
-    'any': tsAnyKeyword,
-    'object': tsObjectKeyword
+    'any': tsUnknownKeyword,
+    'object': tsUnknownKeyword
 };
 
-export const makeTypeDefn = ({ type, properties, items, $ref, enum: _enum }, importSet): TSType => {
+export const makeType = ({ type, properties, items, $ref, enum: _enum }, importSet): TSType => {
     if (properties) {
         return typeLit(properties.map(prop => {
-            const sig = propSig(ident(prop.name), typeAnn(makeTypeDefn(prop, importSet)));
+            const sig = propSig(ident(prop.name), typeAnn(makeType(prop, importSet)));
             sig.optional = !!prop.optional;
-            return sig
+            return sig;
         }));
     } else if (_enum) {
         return unionType(_enum.map(member => litType(stringLiteral(member))));
@@ -54,34 +55,60 @@ export const makeTypeDefn = ({ type, properties, items, $ref, enum: _enum }, imp
         if (names.length > 1) importSet.add(names[0]);
         return typeRef(ident($ref));
     } else if (items) {
-        return arrayType(makeTypeDefn(items, importSet));
+        return arrayType(makeType(items, importSet));
     } else {
         return typesToKWs[type]();
     }
 };
 
-export const makeType = (type, importSet) =>
-    exportNamed(typeAlias(ident(type.id), null, makeTypeDefn(type, importSet)), []);
+export const makeTypeAlias = (type, importSet) =>
+    exportNamed(typeAlias(ident(type.id), null, makeType(type, importSet)), []);
 
-export const makeImports = (importSet: Set<string>) => Array.from(importSet).map(source =>
+export const makeImports = importSet => Array.from(importSet).map((source: string) =>
     importDecl([importNS(ident(source))], stringLiteral(`./${source}`)));
 
-export const makeDomainModule = ({ types }) => {
+export const makeExportFn = ({ name, parameters, returns }, domain, importSet) => {
+    const fnParams = [];
+
+    if (parameters) {
+        const paramsParam = ident('param');
+        paramsParam.typeAnnotation = makeType({ properties: parameters }, importSet);
+        fnParams.push(paramsParam);
+    }
+
+    const contextParam = ident('context');
+    contextParam.typeAnnotation = typeRef(ident('Context'));
+    fnParams.push(contextParam);
+
+    const fn = arrowFunction(fnParams, fnCall(ident('send'), [
+        stringLiteral(`${domain}.${name}`),
+        ident('params'),
+        ident('context'),
+    ]));
+
+    fn.returnType = typeRef(ident('Promise'), typeParams([tsUnknownKeyword()]));
+
+    return exportNamed(variable('const', [binding(ident(name), fn)]));
+};
+
+export const makeDomainModule = ({ domain, types, commands }) => {
     const importSet: Set<string> = new Set();
-    const typeExports = (types ?? []).map(type => makeType(type, importSet));
+    const typeExports = (types ?? []).map(type => makeTypeAlias(type, importSet));
+    const fnExports = (commands ?? []).map(cmd => makeExportFn(cmd, domain, importSet));
     const imports = makeImports(importSet);
 
     return program([
         ...imports,
-        ...typeExports
+        ...typeExports,
+        ...fnExports
     ]);
 };
 
-export const readStdin = () => new Promise<string>((resolve, reject) => {
+export const readStdin = (): Promise<string> => new Promise((resolve, reject) => {
     const stdin = process.stdin;
     let data = '';
     stdin.on('data', chunk => data += chunk);
-    stdin.on('end', () => resolve(data))
+    stdin.on('end', () => resolve(data));
     stdin.on('error', reject);
 });
 
@@ -97,7 +124,7 @@ export const main = async argv => {
 
     protocol.domains.forEach(domain => {
         const domainModule = makeDomainModule(domain);
-        const { code: generatedCode } = generate(domainModule)
+        const { code: generatedCode } = generate(domainModule);
         writeFileSync(join(targetDir, `${domain.domain}.ts`), generatedCode);
     });
 };
@@ -108,7 +135,7 @@ main(yargs(hideBin(process.argv))
             describe: 'Output directory to write the client to',
             type: 'string',
             default: ''
-        })
+        });
     })
     .version('1.0.0')
     .argv);
